a7f7d6fbfdc229518b3c0188a8f6e00b87f1d642
AndhikaRahmatullah <dikarahmatullah909@gmail.com>
2022-11-27 23:00:23 +0700

    feature: navigation - add logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,9 +8,14 @@ const Navigation = () => {
 
 	return (
 		<div className="w-full h-[70px] fixed top-0 px-20 flex flex-row justify-between items-center bg-1 shadow-xl z-50 ">
-			<p className="text-2xl font-bold tracking-wider text-4">
-				XXWAN<span className="px-1 bg-4 text-1">MOPIS</span>
-			</p>
+			<div className="h-[50px] flex flex-row justify-center items-center gap-4">
+				<img
+					src="https://img.icons8.com/plasticine/512/film-reel.png"
+					alt="logo xxwan mopis"
+					className="w-[50px] bg-4 rounded-lg"
+				/>
+				<p className="h-full pr-2 flex items-center text-2xl font-bold tracking-wider text-4">XXWAN MOPIS</p>
+			</div>
 			<nav className="flex flex-row gap-3 text-lg list-none text-gray-700">
 				<li className={splitLocation[1] === "" ? "text-4 transition-all duration-500" : ""}>
 					<Link to="/">Beranda</Link>
